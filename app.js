2d132fadcafb7ee6ea6ca325e1335e797816628e
Sami <sami.mouhmid@gmail.com>
2023-07-09 19:07:35 +0200

    ajout de la page login et test routage/requetage

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const loginRouter = require('./routes/login');
 
 var app = express();
 
@@ -19,9 +20,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/', loginRouter)
+app.use('/index', indexRouter);
 app.use('/users', usersRouter);
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
--- routes/login.js
+++ routes/login.js
@@ -1,8 +1,19 @@
-let router = express.Router();
+const express = require('express');
+const router = express.Router();
 const {PATH_HTML} = require('../public/utils/constantes');
 
 router.get('/', function(req, res, next) {
-    res.sendFile(PATH_HTML + 'login.html');
+    res.redirect('/login')
 });
 
+router.get('/login', function(req, res, next){
+    res.sendFile(PATH_HTML + 'login.html');
+    
+})
+
+router.post('/login', function(req, res, next){
+    res.redirect('/index')
+})
+
 module.exports = router; 
+

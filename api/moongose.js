bd6de0b5901ec30694c5a4a310b4aada762edd4d
Sami <sami.mouhmid@gmail.com>
2023-07-23 16:58:07 +0200

    fin mise en place docker-compose

--- api/moongose.js
+++ api/moongose.js
@@ -1,11 +1,9 @@
 const mongoose = require('mongoose');
 
-    mongoose.connect("mongodb://root:example@localhost:27017/messagerieDB");
+    mongoose.connect("mongodb://sami:sami@localhost:27017/messagerieDB");
+   // mongoose.connect("mongodb://localhost:27017/messagerieDB");
 
-
-
-
-mongoose.connection.on("open", () => console.log("Connexion réussie à la database."))
+    mongoose.connection.on("open", () => console.log("Connexion réussie à la database."))
     mongoose.connection.on("error", () => console.log("Connexion failed à la database."))
 
-module.exports = mongoose;
+    module.exports = mongoose;
---
+++ init-mongo.js
@@ -0,0 +1,13 @@
+db.createUser({
+    user: 'sami',
+    pwd: 'sami',
+    roles: [
+      {
+        role: 'userAdmin',
+        db: 'messagerieDB'
+      }
+    ]
+  })
+
+  //doc : https://stackoverflow.com/questions/57818697/how-to-initialize-a-new-user-and-database-for-mongodb-on-docker-compose
+  //roles : https://www.mongodb.com/docs/manual/reference/built-in-roles/
